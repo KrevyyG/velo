af2d009cb4f8a399e5a0077320c92365950bb899
Guilherme Oliveira Souza <guilhermeos2018@gmail.com>
2026-03-07 17:51:06 -0300

    Novo teste para consulta de pedidos

---
+++ playwright/e2e/pedidos.spec.ts
@@ -0,0 +1,12 @@
+import { test, expect } from "@playwright/test";
+
+test("deve connsultar um pedido aprovado", async ({ page }) => {
+  await page.goto("http://localhost:5173/")
+  await expect(page.getByTestId("hero-section").getByRole("heading")).toContainText("Velô Sprint")
+  await page.getByRole("link", { name: "Consultar Pedido" }).click()
+  await expect(page.getByRole("heading")).toContainText("Consultar Pedido")
+  await page.getByTestId("search-order-id").fill("VLO-ZET8T5")
+  await page.getByTestId('search-order-button').click()
+  await expect(page.getByTestId('order-result-id')).toContainText('VLO-ZET8T5')
+  await expect(page.getByTestId('order-result-status')).toContainText('APROVADO')
+})
