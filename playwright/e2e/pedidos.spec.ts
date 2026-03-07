f4bb44fdec74cda2a92a369c8f5336b005ff28ea
Guilherme Oliveira Souza <guilhermeos2018@gmail.com>
2026-03-07 18:00:32 -0300

    Comentários para AAA

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -1,12 +1,19 @@
 import { test, expect } from "@playwright/test";
 
+///AAA - Arrange, Act, Assert
+
 test("deve consultar um pedido aprovado", async ({ page }) => {
+  // Arrange
   await page.goto("http://localhost:5173/")
   await expect(page.getByTestId("hero-section").getByRole("heading")).toContainText("Velô Sprint")
   await page.getByRole("link", { name: "Consultar Pedido" }).click()
   await expect(page.getByRole("heading")).toContainText("Consultar Pedido")
+  
+  // Act
   await page.getByTestId("search-order-id").fill("VLO-ZET8T5")
   await page.getByTestId('search-order-button').click()
+
+  // Assert
   await expect(page.getByTestId('order-result-id')).toContainText('VLO-ZET8T5')
   await expect(page.getByTestId('order-result-status')).toContainText('APROVADO')
 })
