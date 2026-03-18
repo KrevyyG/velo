8e34ea178360ed57caa9f3dbac64e55546aedbb2
Guilherme Oliveira Souza <guilhermeos2018@gmail.com>
2026-03-18 13:59:12 -0300

    Adição de cenário com busca inválida

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -21,3 +21,27 @@ test("deve consultar um pedido aprovado", async ({ page }) => {
   await expect(orderCode).toBeVisible()
   await expect(page.locator('//div[text()="APROVADO"]')).toBeVisible()
 })
+
+test("deve consultar um pedido inexistente", async ({ page }) => {
+  //Test Data
+  const order = "ABC-DEF1G2"
+
+  // Arrange
+  await page.goto("http://localhost:5173/")
+  await expect(page.getByTestId("hero-section").getByRole("heading")).toContainText("Velô Sprint")
+  await page.getByRole("link", { name: "Consultar Pedido" }).click()
+  await expect(page.getByRole("heading")).toContainText("Consultar Pedido")
+  
+  // Act
+  await page.getByTestId("search-order-id").fill(order)
+  await page.getByRole('button', { name: 'Buscar Pedido' }).click()
+
+  // Assert
+  // await expect(page.getByRole('heading', {name: "Pedido não encontrado"})).toBeVisible()
+  // await expect(page.locator('p', {hasText: "Verifique o número do pedido e tente novamente"})).toBeVisible()
+  await expect(page.locator('#root')).toMatchAriaSnapshot(`
+    - img
+    - heading "Pedido não encontrado" [level=3]
+    - paragraph: Verifique o número do pedido e tente novamente
+    `)
+})
